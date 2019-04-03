66006f5a15b57dfac548ef4ef438d2ada05997e3
Yuefan <361655243@qq.com>
2019-04-03 09:49:30 -0400

    thread per-request

--- EXCHMatchServer.cpp
+++ EXCHMatchServer.cpp
@@ -1,4 +1,41 @@
 #include "exchangeserver.h"
+#include <thread>
+#include <vector>
+void server_func(int newfd) {
+  EXCHANGESERVER Server;
+  Server.handler(newfd);
+  close(newfd);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage:%s <port>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  EXCHANGESERVER Server(argv[1]);
+  if (Server.DBinitializer() == -1) {
+    fprintf(stderr, "failed to initialize database\n");
+  }
+  std::thread t[100];
+  int i = 0;
+  while (1) {
+    int newfd = Server.accNewRequest();
+    // server_func(newfd);
+    t[i++] = std::thread(server_func, newfd);
+    if (i == 100) {
+      for (int j = 0; j < 100; j++)
+        // t[j].detach();
+        t[j].join();
+      i = 0;
+    }
+    // t.detach();
+  }
+  return EXIT_SUCCESS;
+}
+// pre-create thread, contend for same resource
+// it is slow
+
+/*#include "exchangeserver.h"
 #include <mutex>
 #include <queue>
 #include <thread>
@@ -6,7 +43,7 @@
 #define THREAD_NUM 1000
 std::mutex q_mtx;
 std::queue<int> clients;
-void server_func() {
+void server_func_pre_create() {
   while (1) {
     while (clients.empty()) {
     }
@@ -23,7 +60,6 @@ void server_func() {
     close(newfd);
   }
 }
-
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Usage:%s <port>\n", argv[0]);
@@ -33,17 +69,18 @@ int main(int argc, char **argv) {
   if (Server.DBinitializer() == -1) {
     fprintf(stderr, "failed to initialize database\n");
   }
+
   std::thread t[THREAD_NUM];
+
   for (int i = 0; i < THREAD_NUM; i++)
-    t[i] = std::thread(server_func);
+    t[i] = std::thread(server_func_pre_create);
 
   while (1) {
     int newfd = Server.accNewRequest();
-    // server_func(newfd);
     clients.push(newfd);
-    // t.detach();
   }
   for (int j = 0; j < THREAD_NUM; j++)
     t[j].join();
   return EXIT_SUCCESS;
 }
+*/
